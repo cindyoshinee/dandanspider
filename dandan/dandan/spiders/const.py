9483e3a2b714e1c1e1834a1e1ed28e987ea405cc
cindyoshinee <cindyoshinee@sina.com>
2016-07-04 20:36:24 +0800

    spider specially for www.oiegg.com

---
+++ dandan/dandan/spiders/const.py
@@ -0,0 +1,5 @@
+ALLOWED_DOMAINS = ['oiegg.com']
+URL = 'http://www.oiegg.com/'
+DB_CONFIG = {'host':'127.0.0.1', 'user':'root', 'password':'123456', 'db':'dandan', 'port':3306, 'charset':'utf8', 'timeout':5}
+HEADERS = {'user-agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36', 'Host':'www.oiegg.com', 'Connection':'keep-alive'}
+FORM_DATA = {'formhash':'c6505cd5', 'referer':'http://www.oiegg.com/index.php', 'username':'起名这么难', 'password':'123456asd', 'questionid':'0', 'answer':'', 'loginsubmit':'true'}
